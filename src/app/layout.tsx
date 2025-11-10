21e23ae38407236757b5d24584a0063941eb4e3d
Nathaniel Senje <topsailcashew@gmail.com>
2025-11-10 10:24:59 +0000

    these colors are no good. use these instead

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -35,7 +35,7 @@ export default function RootLayout({
       <body className="font-body antialiased">
         <ThemeProvider
           attribute="class"
-          defaultTheme="light"
+          defaultTheme="dark"
           enableSystem
           disableTransitionOnChange
         >
