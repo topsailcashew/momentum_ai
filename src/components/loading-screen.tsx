9038ff8fbac6376b9f80a4f1c5e19a9906c541b4
Nathaniel Senje <topsailcashew@gmail.com>
2025-11-12 08:10:48 +0000

    I dont like the neon glow. make the logo breathe, grow and shrink

--- src/components/loading-screen.tsx
+++ src/components/loading-screen.tsx
@@ -1,13 +1,12 @@
 'use client';
 
 import { Logo } from '@/components/logo';
-import { cn } from '@/lib/utils';
 
 export function LoadingScreen() {
   return (
     <div className="flex h-screen w-screen flex-col items-center justify-center bg-background">
       <div className="relative">
-        <Logo className="h-32 w-32 text-primary animate-glow" />
+        <Logo className="h-32 w-32 text-primary animate-pulse" />
       </div>
     </div>
   );
