6566947c978026f5a9652ecd7819142ad2e9ffbb
Nathaniel Senje <topsailcashew@gmail.com>
2025-11-12 18:09:54 +0000

    Try fixing this error: `Runtime Error: Error: useTransition is not defin

--- src/app/weekly-planner/client-page.tsx
+++ src/app/weekly-planner/client-page.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import * as React from 'react';
+import { useTransition } from 'react';
 import { startOfWeek, endOfWeek, eachDayOfInterval, format, isSameDay, parseISO, addWeeks, subWeeks } from 'date-fns';
 import { Card, CardContent, CardHeader, CardTitle, CardDescription } from '@/components/ui/card';
 import { DayColumn } from '@/components/weekly-planner/day-column';
